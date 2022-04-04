3870ec6a612ce32e15e5bb9557ca9333af34a3a7
Mac Mbizo <macmbizo@gmail.com>
2022-04-05 00:08:55 +0200

    Initial commit - most edits done.

---
+++ mainwindow.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QDialog
+import resource
+from out_window import Ui_OutputDialog
+
+
+class Ui_Dialog(QDialog):
+    def __init__(self):
+        super(Ui_Dialog, self).__init__()
+        loadUi("mainwindow.ui", self)
+
+        self.runButton.clicked.connect(self.runSlot)
+
+        self._new_window = None
+        self.Videocapture_ = None
+
+    def refreshAll(self): # define source of video ('0' is for webcam)
+       self.Videocapture_ = "0"
+
+    @pyqtSlot()
+    def runSlot(self): # call after pressing "Start"
+        print("App started")
+        self.refreshAll()
+        print(self.Videocapture_)
+        ui.hide()  # hide the main window
+        self.outputWindow_()  # Create and open new output window
+
+    def outputWindow_(self): # reset and create a new window for video output ACScreen
+        self._new_window = Ui_OutputDialog()
+        self._new_window.show()
+        self._new_window.startVideo(self.Videocapture_)
+        print("Webcam On")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = Ui_Dialog()
+    ui.show()
+    sys.exit(app.exec_())
